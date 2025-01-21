ec8940fb5dc1c610d4089dbfca11a8ceacb2b827
AlexF2050 <findex2050@gmail.com>
2025-01-21 17:57:18 +0700

    add 1

---
+++ keyboard.py
@@ -0,0 +1,8 @@
+# Кнопки клавиатуры
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main = ReplyKeyboardMarkup(keyboard=[
+   [KeyboardButton(text="Кнопка 1")],
+   [KeyboardButton(text="Кнопка 2"), KeyboardButton(text="Кнопка 3")]
+])
